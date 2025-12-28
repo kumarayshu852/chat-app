7fbda271063615c600382da56ba301d578b9739c
kumarayshu852 <kumarayshu852@gmail.com>
2025-12-28 13:01:14 +0530

    Update useAuthStore.js
    change url

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import {io} from "socket.io-client";
 
 
 
-const BASE_URL= import.meta.env.MODE==="development"? "http://localhost:5001":"/"
+const BASE_URL= import.meta.env.MODE==="development"? "https://chat-app-three-snowy-79.vercel.app/":"/"
 
 export const useAuthStore =create ((set, get)=>({
     authUser:null,
