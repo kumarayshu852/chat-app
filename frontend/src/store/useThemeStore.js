13d54d214a67301d12ad2eac3db9306a244cdfeb
kumarayshu852 <kumarayshu852@gmail.com>
2025-12-28 13:34:26 +0530

    Update useThemeStore.js
    errror fixed it

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,12 +1,9 @@
-import {create} from "zustand";
+import { create } from "zustand";
 
-export const useThemestore =create((set)=>({
-    theme:localStorage.getItem("chat-theme") || "coffee",
-    setTheme:(theme)=>{
-        localStorage.setItem("chat-theme", theme);
-        set({theme});
-    },
-
-    
-
+export const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("chat-theme") || "coffee",
+  setTheme: (theme) => {
+    localStorage.setItem("chat-theme", theme);
+    set({ theme });
+  },
 }));
